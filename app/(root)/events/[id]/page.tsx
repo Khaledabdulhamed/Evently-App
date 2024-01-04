f6d7b16316a0afcda7bf47603e12d4d759038910
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-01-04 09:38:45 -0500

    Adding many featurs

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,13 +1,20 @@
-import { getEventById } from '@/lib/actions/event.actions'
+import Collection from '@/components/shared/Collection'
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils'
 import { SearchParamProps } from '@/types'
 import Image from 'next/image'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
-const EventsDetails = async({params: {id}}:SearchParamProps) => {
+const EventsDetails = async({params: {id}, searchParams}:SearchParamProps) => {
   const event = await getEventById(id)
-  console.log(event)
+  const realtedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string
+  })
   return (
+    <>
     <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
       <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
         <Image
@@ -67,6 +74,19 @@ const EventsDetails = async({params: {id}}:SearchParamProps) => {
         </div>
       </div>
     </section>
+    {/* Events with the same category */}
+    <section className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
+    <h2 className='h2-bold'>Related Events</h2>
+    <Collection
+          data={realtedEvents?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back Later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2} urlParamName={''}    />
+    </section>
+    </>
   )
 }
 
